03e38db1bac71ddc24bc0b5a381c08f78a1e7d3f
Justin Foss <jfoss124@gmail.com>
2015-10-15 16:05:04 -0700

    Changed config to allow for optional autofixing or not

--- jsStyleCheck.js
+++ jsStyleCheck.js
@@ -4,26 +4,43 @@ var fs = require('fs');
 var fixMyJs = require('fixmyjs');
 var jshint = require('jshint').JSHINT;
 var JSCS = require('jscs');
-var glob = require('glob');
 var clc = require('cli-color');
 var checker = new JSCS();
 
+// initializations
 var exitCode = 0;
-var filePaths = process.argv.slice(2, process.argv.length - 2);
-var configFlag = process.argv[process.argv.length - 1];
-var config = undefined;
+var args = process.argv.slice(2);
+var fixFlag = false;
+var whiteSpaceFlag = false;
+var filePaths;
 
-if (configFlag === "-w") {
-  config = 'volt-whitespace';
-} else {
-  config = 'volt-full-lint'
+// find flag arguments, if any
+for (var j = 0; j < args.length; j++){
+  if (args[j] === '-f') fixFlag = true;
+  if (args[j] === '-w') whiteSpaceFlag = true;
 }
 
-checker.registerDefaultRules();
-checker.configure({
-  preset: config
-});
+// parse the filepaths out of the arguments
+if ( fixFlag && whiteSpaceFlag ) {
+ filePaths = process.argv.slice(2, -2);
+} else if ( (fixFlag && !whiteSpaceFlag) || (whiteSpaceFlag && !fixFlag) ) {
+  filePaths = process.argv.slice(2, -1);
+} else {
+  filePaths = process.argv.slice(2);
+}
 
+// set the correct config based on the presence/lack of -w flag
+if (whiteSpaceFlag) {
+  config = require('./config/volt_whitespace.json');
+} else {
+  config = require('./config/volt_full_lint.json');
+}
+
+// initialize linter with rules
+checker.registerDefaultRules();
+checker.configure(config);
+
+// options object for fixmyjs
 var fmjsOptions = {
   camelcase: true,
   plusplus: true,
@@ -31,47 +48,84 @@ var fmjsOptions = {
   eqeqeq: true,
 };
 
-
+// loop through each filepath in filpaths array and take appropriate
+// actions based on the presence/lack of flag arguments
 for (var i = 0; i < filePaths.length; i += 1) {
 
   var rawCode = fs.readFileSync(filePaths[i]).toString();
   var linted = checker.checkString(rawCode);
   var lintErrors = linted.getErrorList();
-  var linterFixed = undefined;
 
-  if (configFlag === "-w") {
-    linterFixed = checker.fixString(rawCode, filePaths[i]);
-  } else {
-    var fmjsFixed = fixMyJs.fix(rawCode, fmjsOptions);
-    linterFixed = checker.fixString(fmjsFixed, filePaths[i]);
-  }
-
+  // reporter header for each file
   console.log(clc.yellow(clc.white('\nLinting ') + filePaths[i]));
   console.log(clc.blue('***************************************************************'));
 
-  if (lintErrors.length > 0) {
+  // if "-f" was present in arguments, run the lint and fix operation
+  if (fixFlag) {
+    var linterFixed = undefined;
     
-    if (linterFixed && linterFixed.output.length > 0) {
-      fs.writeFileSync(filePaths[i], linterFixed.output);
-      console.log(clc.magenta(clc.white('Auto fixing ') + filePaths[i] + '\n\n'));
+    if (whiteSpaceFlag) {
+      linterFixed = checker.fixString(rawCode, filePaths[i]);
+    } else {
+      var fmjsFixed = fixMyJs.fix(rawCode, fmjsOptions);
+      linterFixed = checker.fixString(fmjsFixed, filePaths[i]);
     }
 
-    var postFixLinted = checker.checkString(linterFixed.output);
-    var postFixLintErrors = postFixLinted.getErrorList();
+    // run auto fix if we have lint errors
+    if (lintErrors.length > 0) {
 
-    if (postFixLintErrors.length > 0){
-      exitCode = 1;
-      for (var n = 0; n < postFixLintErrors.length; n += 1) {
-        var colorizedOutput = true;
-        console.log(clc.green("Remaining errors after autofix----------------"));
-        console.log(linted.explainError(postFixLintErrors[n], colorizedOutput) + '\n');
+      if (linterFixed && linterFixed.output.length > 0) {
+        autoFix(filePaths[i], linterFixed.output);
       }
+
+      var postFixLinted = checker.checkString(linterFixed.output);
+      var postFixLintErrors = postFixLinted.getErrorList();
+
+      // re-lint after auto fixing and see if we have remaining errors
+      if (postFixLintErrors.length > 0){
+        exitCode = 1;
+        
+        for (var n = 0; n < postFixLintErrors.length; n += 1) {
+          var colorizedOutput = true;
+          console.log(clc.green("Remaining errors after autofix----------------"));
+          console.log(linted.explainError(postFixLintErrors[n], colorizedOutput) + '\n');
+        }
+      }
+    } else {
+      console.log(clc.greenBright('No lintable errors!  ' + clc.yellowBright('d=(´▽ `)=b\n')));
     }
   } else {
-    console.log(clc.greenBright('No lintable errors!  ' + clc.yellowBright('d=(´▽ `)=b\n')));
+    if (lintErrors.length > 0){
+      exitCode = 1;
+      
+      for (var n = 0; n < lintErrors.length; n += 1) {
+        var colorizedOutput = true;
+        console.log(clc.green("Linter found errors ----------------"));
+        console.log(linted.explainError(postFixLintErrors[n], colorizedOutput) + '\n');
+      }
+    } else {
+      console.log(clc.greenBright('No lintable errors!  ' + clc.yellowBright('d=(´▽ `)=b\n')));
+    }
   }
+
+console.log('Process exited with code ' +(exitCode ? '1 ' + clc.cyan('(ノಠ益ಠ)ノ') : '0 ' +  clc.yellowBright('(/^ O ^)/')));
+process.exit(exitCode);
+
+
+/*****************************************************
+                      HELPERS
+*****************************************************/
+
+function autoFix(filePath, file){
+  fs.writeFileSync(filePath, file);
+  console.log(clc.magenta(clc.white('Auto fixing ') + filePath + '\n\n'));
 }
 
-console.log('Process exited with code ' +(exitCode ? '1 ' + clc.cyan('(ノಠ益ಠ)ノ') : '0 ' +  clc.yellowBright('(/^ ▽ ^)/')));
-process.exit(exitCode);
+
+
+
+
+
+
+
 
